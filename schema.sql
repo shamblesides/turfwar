54ad8de3302681f34c3c51ee11e12e1bb6b011b8
Nigel Nelson <nigel.mark.nelson@gmail.com>
2022-09-08 16:33:29 -0600

    don't restrict name length at database schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "land" (
     ip INTEGER PRIMARY KEY,
-    nick TEXT NOT NULL CHECK (length(nick) = 8),
+    nick TEXT NOT NULL CHECK,
     created_at TEXT DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW'))
 );
