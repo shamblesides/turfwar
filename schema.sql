f5a3c4e574d36c4a8ff8924949481922190ea4dc
Nigel Nelson <nigel.mark.nelson@gmail.com>
2022-09-15 22:21:13 -0600

    created_at is not necessary

--- schema.sql
+++ schema.sql
@@ -1,7 +1,6 @@
 CREATE TABLE "land" (
     ip INTEGER PRIMARY KEY,
-    nick TEXT NOT NULL,
-    created_at TEXT DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW'))
+    nick TEXT NOT NULL
 );
 
 CREATE INDEX "land_by_name" ON "land" (nick);
