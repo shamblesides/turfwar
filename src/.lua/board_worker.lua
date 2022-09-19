82a424dc3b8971e6e524431e1127f0f54465e928
Nigel Nelson <nigel.mark.nelson@gmail.com>
2022-09-19 16:38:52 -0600

    don't commit partially complete board queries

--- src/.lua/board_worker.lua
+++ src/.lua/board_worker.lua
@@ -5,32 +5,8 @@
     update it periodically and cache it instead.
 ]]
 
-local function UpdateBoardCrawl(db, output, stmt)
-    local smallest, res
-    for smallest = 0, 0xFF000000, 0x1000000 do
-        if stmt:bind_values(smallest, smallest + 0xFFFFFF) ~= sqlite3.OK then
-            Log(kLogWarn, "BOARD Internal error (stmt:bind_values): %s" % {db:errmsg()})
-            return
-        end
-        res = stmt:step()
-        if res == sqlite3.ROW then
-            table.insert(output["leaders"], stmt:get_named_values())
-            if stmt:step() ~= sqlite3.DONE then
-                Log(kLogWarn, "BOARD Internal error (stmt:step after row): %s" % {db:errmsg()})
-                return
-            end
-        elseif res == sqlite3.DONE then
-            table.insert(output["leaders"], false)
-        else
-            Log(kLogWarn, "BOARD Internal error (stmt:step returned %d): %s" % {res, db:errmsg()})
-            return
-        end
-        stmt:reset()
-    end
-end
-
 local function UpdateBoardImpl(db)
-    local stmt, err, output
+    local stmt, err, smallest, res, output, success
     stmt, err = db:prepare([[
         SELECT nick As name, COUNT(ip) AS count
         FROM land
@@ -43,24 +19,50 @@ local function UpdateBoardImpl(db)
         Log(kLogWarn, "BOARD prepare board query: %s / %s" % {err or "(null)", db:errmsg()})
         return
     end
+
     output = {["leaders"]={}, ["now"]=os.time()}
-    UpdateBoardCrawl(db, output, stmt)
-    stmt:finalize()
-    stmt, err = db:prepare([[
-        INSERT INTO cache (key, val) VALUES ('/board', ?1)
-        ON CONFLICT (key) DO UPDATE SET (val) = (?1)
-    ]])
-    if not stmt then
-        Log(kLogWarn, "BOARD prepare insert: %s / %s" % {err or "(null)", db:errmsg()})
-        return
-    elseif stmt:bind_values(EncodeJson(output)) ~= sqlite3.OK then
-        Log(kLogWarn, "BOARD insert bind: %s" % {db:errmsg()})
-    elseif stmt:step() ~= sqlite3.DONE then
-        Log(kLogWarn, "BOARD insert step: %s" % {db:errmsg()})
-    else
-        Log(kLogInfo, "BOARD successfully updated")
+    err = nil
+    for smallest = 0, 0xFF000000, 0x1000000 do
+        if stmt:bind_values(smallest, smallest + 0xFFFFFF) ~= sqlite3.OK then
+            err = "BOARD Internal error (stmt:bind_values): %s" % {db:errmsg()}
+            break
+        end
+        res = stmt:step()
+        if res == sqlite3.ROW then
+            table.insert(output["leaders"], stmt:get_named_values())
+            if stmt:step() ~= sqlite3.DONE then
+                err = "BOARD Internal error (stmt:step after row): %s" % {db:errmsg()}
+                break
+            end
+        elseif res == sqlite3.DONE then
+            table.insert(output["leaders"], false)
+        else
+            err = "BOARD Internal error (stmt:step returned %d): %s" % {res, db:errmsg()}
+            break
+        end
+        stmt:reset()
     end
     stmt:finalize()
+
+    if not err then
+        stmt, err = db:prepare([[
+            INSERT INTO cache (key, val) VALUES ('/board', ?1)
+            ON CONFLICT (key) DO UPDATE SET (val) = (?1)
+        ]])
+        if not stmt then
+            Log(kLogWarn, "BOARD prepare insert: %s / %s" % {err or "(null)", db:errmsg()})
+            return
+        elseif stmt:bind_values(EncodeJson(output)) ~= sqlite3.OK then
+            Log(kLogWarn, "BOARD insert bind: %s" % {db:errmsg()})
+        elseif stmt:step() ~= sqlite3.DONE then
+            Log(kLogWarn, "BOARD insert step: %s" % {db:errmsg()})
+        else
+            Log(kLogInfo, "BOARD successfully updated")
+        end
+        stmt:finalize()
+    else
+        Log(kLogWarn, err)
+    end
 end
 
 local gotterm = false
